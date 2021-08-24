a85d5bcbfcfa277ce8ce38f86bc85373858b5c99
tshifhiwa <tdmadima@gamil.com>
2021-08-24 18:00:02 +0200

    view

--- demo/urls.py
+++ demo/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 from django.urls import path, include
+from django.contrib import admin
+
+
 
 urlpatterns = [
     path('demo/', include('demo.urls')),
--- first/urls.py
+++ first/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path
+from views import first
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path,include
 
 urlpatterns = [
     path('demo/', include('demo.urls')),
     path('admin/', admin.site.urls),
-    path('', views.first),
+    path('first', first),
 ]
+
